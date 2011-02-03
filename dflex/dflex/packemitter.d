95240de146ae5f23e0b9b8b31a98554a875a22ba
Robert BuRnEr Schadek <rburners@gmail.com>
2011-02-03 19:34:32 +0100

    indent

--- dflex/dflex/packemitter.d
+++ dflex/dflex/packemitter.d
@@ -35,183 +35,183 @@ import hurt.string.stringbuffer;
  * @author Gerwin Klein */
 public abstract class PackEmitter {
 
-  /** name of the generated array (mixed case, no yy prefix) */
-  protected string name;
-    
-  /** current UTF8 length of generated string in current chunk */
-  private int UTF8LengthVar;
+	/** name of the generated array (mixed case, no yy prefix) */
+	protected string name;
 
-  /** position in the current line */
-  private int linepos;
-  
-  /** max number of entries per line */
-  private static immutable maxEntries = 16;
-  
-  /** output buffer */
-  protected StringBuffer!(char) outsb;
+	/** current UTF8 length of generated string in current chunk */
+	private int UTF8LengthVar;
 
-  /** number of existing string chunks */ 
-  protected int chunks;
-    
-  /** maximum size of chunks */
-  // String constants are stored as UTF8 with 2 bytes length
-  // field in class files. One Unicode char can be up to 3 
-  // UTF8 bytes. 64K max and two chars safety. 
-  private static immutable maxSize = 0xFFFF-6;
-  
-  /** indent for string lines */
-  private static immutable indent = "    ";
-  
-  /**
-   * Create new emitter for an array.
-   * 
-   * @param name  the name of the generated array
-   */
-  public this(string name) {
-	this.outsb = new StringBuffer!(char)(16);
-    this.name = name;
-  }
-  
-  /**
-   * Convert array name into all uppercase internal scanner 
-   * constant name.
-   * 
-   * @return <code>name</code> as a internal constant name.
-   * @see PackEmitter#name
-   */
-  protected string constName() {
-    return "ZZ_" ~ name.toUpperCase();
-  }
-  
-  /**
-   * Return current output buffer.
-   */
-  public override string toString() {
-    return outsb.toString();
-  }
+	/** position in the current line */
+	private int linepos;
 
-  /**
-   * Emit declaration of decoded member and open first chunk.
-   */  
-  public void emitInit() {
-    outsb.append("  private static final int [] ");
-    outsb.append(constName());
-    outsb.append(" = zzUnpack");
-    outsb.append(name);
-    outsb.append("();");
-    outsb.append("\n");
-    nextChunk();
-  }
+	/** max number of entries per line */
+	private static immutable maxEntries = 16;
 
-  /**
-   * Emit single unicode character. 
-   * 
-   * Updates length, position, etc.
-   *
-   * @param i  the character to emit.
-   * @prec  0 <= i <= 0xFFFF 
-   */   
-  public void emitUC(int i) {     
-    if (i < 0 || i > 0xFFFF) 
-      throw new IllegalArgumentException("character value expected");
-  
-    // cast ok because of prec  
-    char c = cast(char)i;    
-     
-    printUC(c);
-    UTF8Lengthvar += UTF8Length(c);
-    linepos++;   
-  }
+	/** output buffer */
+	protected StringBuffer!(char) outsb;
 
-  /**
-   * Execute line/chunk break if necessary. 
-   * Leave space for at least two chars.
-   */  
-  public void breaks() {
-    if (UTF8Lengthvar >= maxSize) {
-      // close current chunk
-      outsb.append("\";");
-      outsb.append("\n");
-      
-      nextChunk();
-    }
-    else {
-      if (linepos >= maxEntries) {
-        // line break
-        outsb.append("\"+");
-        outsb.append("\n");
-        outsb.append(indent);
-        outsb.append("\"");
-        linepos = 0;      
-      }
-    }
-  }
-  
-  /**
-   * Emit the unpacking code. 
-   */
-  public abstract void emitUnpack();
+	/** number of existing string chunks */ 
+	protected int chunks;
 
-  /**
-   *  emit next chunk 
-   */
-  private void nextChunk() {
-    outsb.append("\n");
-    outsb.append("  private static final String ");
-    outsb.append(constName());
-    outsb.append("_PACKED_");
-    outsb.append(chunks);
-    outsb.append(" =");
-    outsb.append("\n");
-    outsb.append(indent);
-    outsb.append("\"");
+	/** maximum size of chunks */
+	// String constants are stored as UTF8 with 2 bytes length
+	// field in class files. One Unicode char can be up to 3 
+	// UTF8 bytes. 64K max and two chars safety. 
+	private static immutable maxSize = 0xFFFF-6;
 
-    UTF8Length = 0;
-    linepos = 0;
-    chunks++;
-  }
-  
-  /**
-   * Append a unicode/octal escaped character 
-   * to <code>out</code> buffer.
-   * 
-   * @param c the character to append
-   */
-  private void printUC(char c) {
-    if (c > 255) {
-      outsb.append("\\u");
-      if (c < 0x1000) outsb.append("0");
-      outsb.append(Integer.toHexString(c));
-    }
-    else {
-      outsb.append("\\");
-      outsb.append(Integer.toOctalString(c));
-    }
-  } 
+	/** indent for string lines */
+	private static immutable indent = "    ";
 
-  /**
-   * Calculates the number of bytes a Unicode character
-   * would have in UTF8 representation in a class file.
-   *
-   * @param value  the char code of the Unicode character
-   * @prec  0 <= value <= 0xFFFF
-   *
-   * @return length of UTF8 representation.
-   */
-  private int UTF8Length(char value) {
-    // if (value < 0 || value > 0xFFFF) throw new Error("not a char value ("+value+")");
+	/**
+	 * Create new emitter for an array.
+	 * 
+	 * @param name  the name of the generated array
+	 */
+	public this(string name) {
+		this.outsb = new StringBuffer!(char)(16);
+		this.name = name;
+	}
 
-    // see JVM spec section 4.4.7, p 111
-    if (value == 0) return 2;
-    if (value <= 0x7F) return 1;
+	/**
+	 * Convert array name into all uppercase internal scanner 
+	 * constant name.
+	 * 
+	 * @return <code>name</code> as a internal constant name.
+	 * @see PackEmitter#name
+	 */
+	protected string constName() {
+		return "ZZ_" ~ name.toUpperCase();
+	}
 
-    // workaround for javac bug (up to jdk 1.3):
-    if (value <  0x0400) return 2;
-    if (value <= 0x07FF) return 3;
+	/**
+	 * Return current output buffer.
+	 */
+	public override string toString() {
+		return outsb.toString();
+	}
 
-    // correct would be:
-    // if (value <= 0x7FF) return 2;
-    return 3;
-  }
+	/**
+	 * Emit declaration of decoded member and open first chunk.
+	 */  
+	public void emitInit() {
+		outsb.append("  private static final int [] ");
+		outsb.append(constName());
+		outsb.append(" = zzUnpack");
+		outsb.append(name);
+		outsb.append("();");
+		outsb.append("\n");
+		nextChunk();
+	}
+
+	/**
+	 * Emit single unicode character. 
+	 * 
+	 * Updates length, position, etc.
+	 *
+	 * @param i  the character to emit.
+	 * @prec  0 <= i <= 0xFFFF 
+	 */   
+	public void emitUC(int i) {     
+		if (i < 0 || i > 0xFFFF) 
+			throw new IllegalArgumentException("character value expected");
+
+		// cast ok because of prec  
+		char c = cast(char)i;    
+
+		printUC(c);
+		UTF8Lengthvar += UTF8Length(c);
+		linepos++;   
+	}
+
+	/**
+	 * Execute line/chunk break if necessary. 
+	 * Leave space for at least two chars.
+	 */  
+	public void breaks() {
+		if (UTF8Lengthvar >= maxSize) {
+			// close current chunk
+			outsb.append("\";");
+			outsb.append("\n");
+
+			nextChunk();
+		}
+		else {
+			if (linepos >= maxEntries) {
+				// line break
+				outsb.append("\"+");
+				outsb.append("\n");
+				outsb.append(indent);
+				outsb.append("\"");
+				linepos = 0;      
+			}
+		}
+	}
+
+	/**
+	 * Emit the unpacking code. 
+	 */
+	public abstract void emitUnpack();
+
+	/**
+	 *  emit next chunk 
+	 */
+	private void nextChunk() {
+		outsb.append("\n");
+		outsb.append("  private static final String ");
+		outsb.append(constName());
+		outsb.append("_PACKED_");
+		outsb.append(chunks);
+		outsb.append(" =");
+		outsb.append("\n");
+		outsb.append(indent);
+		outsb.append("\"");
+
+		UTF8Length = 0;
+		linepos = 0;
+		chunks++;
+	}
+
+	/**
+	 * Append a unicode/octal escaped character 
+	 * to <code>out</code> buffer.
+	 * 
+	 * @param c the character to append
+	 */
+	private void printUC(char c) {
+		if (c > 255) {
+			outsb.append("\\u");
+			if (c < 0x1000) outsb.append("0");
+			outsb.append(Integer.toHexString(c));
+		}
+		else {
+			outsb.append("\\");
+			outsb.append(Integer.toOctalString(c));
+		}
+	} 
+
+	/**
+	 * Calculates the number of bytes a Unicode character
+	 * would have in UTF8 representation in a class file.
+	 *
+	 * @param value  the char code of the Unicode character
+	 * @prec  0 <= value <= 0xFFFF
+	 *
+	 * @return length of UTF8 representation.
+	 */
+	private int UTF8Length(char value) {
+		// if (value < 0 || value > 0xFFFF) throw new Error("not a char value ("+value+")");
+
+		// see JVM spec section 4.4.7, p 111
+		if (value == 0) return 2;
+		if (value <= 0x7F) return 1;
+
+		// workaround for javac bug (up to jdk 1.3):
+		if (value <  0x0400) return 2;
+		if (value <= 0x07FF) return 3;
+
+		// correct would be:
+		// if (value <= 0x7FF) return 2;
+		return 3;
+	}
 
 }
